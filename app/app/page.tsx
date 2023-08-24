5d0e9423ccde68ad3206d6a85a3e2a483064bf50
Art Dev <devtest33333@gmail.com>
2023-08-24 15:08:52 +0300

    Basic TODO view. Data fetching

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,3 +1,31 @@
-export default function Home() {
-    return <h1>Hi</h1>
+import { prisma } from './db'
+import  Link from 'next/link'
+import {TodoItem} from "../components/TodoItem";
+
+function getTodos() {
+    return prisma.todo.findMany()
+}
+
+export default async function Home() {
+    const todos = await getTodos()
+    // await prisma.todo.create({ data: { title: "Test", complete: false }})
+
+    return (
+        <>
+            <header className="flex justify-between items-center mb-4">
+                <h1 className="text-2xl">Todos</h1>
+                <Link
+                    href="/new"
+                    className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+                >
+                    New
+                </Link>
+            </header>
+            <ul className="pl-4">
+                {todos.map(todo => (
+                    <TodoItem key={todo.id} {...todo} />
+                ))}
+            </ul>
+        </>
+    )
 }
